97143275993abdf87c9b604445b8934376183553
Moses Mulwa <mosesmulwa123@gmail.com>
2022-01-20 11:35:30 +0300

    Added MapBase Class

---
+++ Maps,Hash Tables and Skip Lists/Maps/MapBase.py
@@ -0,0 +1,23 @@
+from collections.abc import MutableMapping
+
+
+class MapBase(MutableMapping):
+    """Our own abstract base class that includes a nonpublic Item class."""
+
+    #  ------------------------------- nested Item class -------------------------------
+    class Item:
+        """Lightweight composite to store key-value pairs as map items."""
+        __slots__ = '_key', '_value'
+
+    def __init__(self, k, v):
+        self. key = k
+        self. value = v
+
+    def __eq__(self, other):
+        return self. key == other. key    # compare items based on their keys
+
+    def __ne__(self, other):
+        return not (self == other)        # opposite of eq
+
+    def __lt__(self, other):
+        return self.key < other.key       # compare items based on their keys
